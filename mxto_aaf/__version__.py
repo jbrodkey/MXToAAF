90c283541555d1f84df563fb80ba76f8bea4176c
Jason Brodkey <jasonbrodkey@Mac.attlocal.net>
2025-12-07 21:45:44 -0800

    Bump version to 1.0.0

--- mxto_aaf/__version__.py
+++ mxto_aaf/__version__.py
@@ -1,5 +1,5 @@
 """Version information for MXToAAF"""
 
-__version__ = "0.9.0"
+__version__ = "1.0.0"
 __author__ = "Jason Brodkey"
 __description__ = "Convert music files to embedded AAFs for Avid with metadata extraction"
